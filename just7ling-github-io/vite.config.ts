43f40489e930fd5892a22f8892053d63dcc6551c
이주영 <jylee9931@gmail.com>
2026-01-10 13:04:05 +0900

    add settings

--- just7ling-github-io/vite.config.ts
+++ just7ling-github-io/vite.config.ts
@@ -4,9 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/JUST7LING.github.io/",
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: "built",
   },
 });
 

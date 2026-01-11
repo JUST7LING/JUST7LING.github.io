053e3132492d9037c8044fd86fbb18fdd210db8c
이주영 <jylee9931@gmail.com>
2026-01-11 14:02:41 +0900

    edited Main.tsx & draft layout

--- just7ling-github-io/src/pages/Main.tsx
+++ just7ling-github-io/src/pages/Main.tsx
@@ -6,22 +6,31 @@ export const Main = () => {
       className="
       bg-portfolio 
       min-h-screen min-w-screen 
-      flex flex-col
+      p-[3vw]
+      flex flex-col gap-[8vh]
     "
     >
       <header className="flex justify-start">
         <img src={Signature} className="w-[3vw]" />
       </header>
+
       <article
         className="
-        font-pretendard font-bold text-9xl
-        flex flex-col items-center justify-center
+        font-pretendard font-semibold text-9xl
+        flex flex-col items-center justify-center 
         "
       >
-        <div className="w-[75vw] flex justify-start">HO!</div>
-        <div className="w-[75vw] flex justify-end">HOORAY!</div>
+        <div className="w-[75vw] flex justify-start">Hello, I'm</div>
+        <div className="w-[75vw] flex justify-end">Lee JooYoung!</div>
       </article>
-      <footer className="flex justify-end">
+      <section className="flex justify-center gap-[10vw]">
+        <img src={Signature} className="w-[8vw]" />
+        <img src={Signature} className="w-[8vw]" />
+        <img src={Signature} className="w-[8vw]" />
+        <img src={Signature} className="w-[8vw]" />
+      </section>
+
+      <footer className="flex flex-col justify-end items-end">
         <img src={Signature} className="w-[3vw]" />
       </footer>
     </div>
